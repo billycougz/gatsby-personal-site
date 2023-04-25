c6f943a6c1962d494b72a2fb9da301f07d3ba1c3
billycougz <billycougz@gmail.com>
2023-04-24 20:10:47 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `William Cougan`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
