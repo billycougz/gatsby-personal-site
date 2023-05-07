78d51aeb941311d17db651c36ff2697269ababdf
billycougz <billycougz@gmail.com>
2023-05-07 11:57:22 -0400

    window undefined check

--- src/components/Marquee.js
+++ src/components/Marquee.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
 const Marquee = () => {
-	const [position, setPosition] = useState(window.innerWidth);
+	const [position, setPosition] = useState(typeof window !== 'undefined' ? window.innerWidth : 0);
 
 	const text = `Check out my Twitter to test your Mario knowledge! Every day at 9:00 AM EDT my Mario Quiz Bot asks OpenAI (ChatGPT) to generate a quiz, then the bot posts the quiz as a Twitter poll and ultimately posts the answer at 5:00 PM EDT. Try it out and turn on notifications to be reminded every day!`;
 
