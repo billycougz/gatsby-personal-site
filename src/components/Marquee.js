86393122be0f8d96575b75cb179c22bfac3a8bc5
billycougz <billycougz@gmail.com>
2023-05-21 20:36:10 -0400

    Content tweak

--- src/components/Marquee.js
+++ src/components/Marquee.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 const Marquee = () => {
 	const [position, setPosition] = useState(typeof window !== 'undefined' ? window.innerWidth : 0);
 
-	const text = `Check out my Twitter to test your Mario knowledge! Every day at 9:00 AM EDT my Mario Quiz Bot asks OpenAI (ChatGPT) to generate a quiz, then the bot posts the quiz as a Twitter poll and ultimately posts the answer at 5:00 PM EDT. Try it out and turn on notifications to be reminded every day!`;
+	const text = `Visit my Twitter to take my AI-generated Mario quiz! Every day at 9:00 AM EDT my Mario Quiz Bot asks OpenAI (ChatGPT) to generate a quiz, then the bot posts the quiz as a Twitter poll and ultimately posts the answer at 5:00 PM EDT. Try it out and turn on notifications from my profile for a daily reminder!`;
 
 	useEffect(() => {
 		const interval = setInterval(() => {
