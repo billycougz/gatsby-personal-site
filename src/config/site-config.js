8b03a44a4386b4884d46eded988eb9a279a39253
William Cougan <billycougz@gmail.com>
2025-11-28 21:24:12 -0500

    Update marquee text in site-config.js

--- src/config/site-config.js
+++ src/config/site-config.js
@@ -1,5 +1,5 @@
 const siteConfig = {
-  marquee: "Welcome! I'm an engineer with a background in web development and AI engineering. Reach out and let me know you stopped by :)"
+  marquee: "Welcome! I'm an engineer with a background in Web and AI. Reach out and let me know you stopped by :)"
 };
 
 export default siteConfig;
