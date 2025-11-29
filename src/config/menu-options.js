861adc4d4cb94bcbb8189f8b22053a106b38e8e1
William Cougan <billycougz@gmail.com>
2025-11-28 20:07:31 -0500

    Reorder menu options and update labels

---
+++ src/config/menu-options.js
@@ -0,0 +1,21 @@
+const menuOptions = {
+  default: [
+    { type: "external", label: "1 LINKEDIN", href: "https://www.linkedin.com/in/williamcougan/" },
+    { type: "external", label: "2 GITHUB", href: "https://github.com/billycougz" },
+    { type: "submenu", label: "3 CONCEPTS", submenu: "concepts" },
+    { type: "submenu", label: "4 APPS", submenu: "apps" }
+  ],
+  concepts: [
+    { type: "internal", label: "1 FLAPPY TANOOKI", to: "/concept/Flappy-Tanooki" },
+    { type: "internal", label: "2 DAY AND NITE", to: "/concept/Day-And-Nite" },
+    { type: "back", label: "← MAIN MENU", submenu: "default" }
+  ],
+  apps: [
+    { type: "external", label: "1 PERFECTIONLISTS", href: "https://perfectionlists.billycougan.com/" },
+    { type: "external", label: "2 SQUARES", href: "https://squares.billycougan.com/" },
+    { type: "external", label: "3 BILLSEYE", href: "https://sheets.billycougan.com/" },
+    { type: "back", label: "← MAIN MENU", submenu: "default" }
+  ]
+};
+
+export default menuOptions;
